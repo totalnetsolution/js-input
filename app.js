1a4580014cb4f6c109e25fd689249cf77deca1c3
muhammad-danish <totalnetsolution2@gmail.com>
2024-03-08 02:15:52 +0500

    index file created

---
+++ app.js
@@ -0,0 +1,39 @@
+// console.log('hello world');
+
+// function greetuser (user){
+//     console.log('Hello ' + user)
+// }
+
+// var username = greetuser('Muhammad Danish');
+//     console.log
+
+
+
+//     var head = document.querySelector('.head');
+// function greetuser(){
+// console.log(head.innerHTML);
+// head.innerHTML = 'Hello world';
+// if (head.innerHTML === 'Hello World') {
+//     head.innerHTML = 'changed through js'
+// } else {head.innerHTML = 'changed through js'
+// }
+
+// }
+
+
+var  input = document.querySelector('.input');
+    
+function getInputValue() {
+    console.log(input.value)
+    input.value = ' '
+}
+
+
+var firstName = 'muhammad';
+var lastName = 'danish';
+var age = 40;
+
+console.log(`my name is ${firstName} ${lastName} and my age is ${age} `);
+
+
+// 4 form field banaingi. 4 subjects ka number user daiga. total marks 400. button ka click paa uska obtained marks, percentage and grade.
